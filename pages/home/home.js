3839e7f0e4e9e897e1c8d29b22b1a028bdfd56ff
ntl <1572679105@qq.com>
2023-02-28 20:23:45 +0800

    主页初见成效

---
+++ pages/home/home.js
@@ -0,0 +1,54 @@
+var mySwiperone = new Swiper('.swiper-one', {
+    loop: true, // 循环模式选项
+    parallax : true,
+
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // autoplay: {
+    //     // delay: 2000,//1秒切换一次
+    // },
+}) 
+
+var mySwipertwo = new Swiper('.swiper-two', {
+    loop: true, // 循环模式选项
+    parallax : true,
+
+    slidesPerView: 6,
+    grid: {
+        fill: 'column',
+        rows: 1,
+    },
+    autoplay: {
+        // delay: 2000,//1秒切换一次
+    },
+})    
+
+    
+var mySwiperThree = new Swiper ('.swiper-three', {
+    loop: true,
+    loopAdditionalSlides: 3,
+    
+    // 如果需要分页器
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    slidesPerView: 3.5,
+    grid: {
+        fill: 'column',
+        rows: 1,
+    },
+    
+    //  如果需要前进后退按钮
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+    
+    // 如果需要滚动条
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+})        
