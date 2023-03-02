37f614c0ddfa0f673a9a199f3fe1d9e8e69eff1e
ntl <1572679105@qq.com>
2023-03-02 20:12:54 +0800

    主页面完成

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,6 @@
 var mySwiperone = new Swiper('.swiper-one', {
     loop: true, // 循环模式选项
-    parallax : true,
+    parallax: true,
 
     // 如果需要前进后退按钮
     navigation: {
@@ -10,11 +10,11 @@ var mySwiperone = new Swiper('.swiper-one', {
     // autoplay: {
     //     // delay: 2000,//1秒切换一次
     // },
-}) 
+})
 
 var mySwipertwo = new Swiper('.swiper-two', {
     loop: true, // 循环模式选项
-    parallax : true,
+    parallax: true,
 
     slidesPerView: 6,
     grid: {
@@ -24,13 +24,13 @@ var mySwipertwo = new Swiper('.swiper-two', {
     autoplay: {
         // delay: 2000,//1秒切换一次
     },
-})    
+})
 
-    
-var mySwiperThree = new Swiper ('.swiper-three', {
+
+var mySwiperThree = new Swiper('.swiper-three', {
     loop: true,
     loopAdditionalSlides: 3,
-    
+
     // 如果需要分页器
     pagination: {
         el: '.swiper-pagination',
@@ -40,15 +40,52 @@ var mySwiperThree = new Swiper ('.swiper-three', {
         fill: 'column',
         rows: 1,
     },
-    
+
     //  如果需要前进后退按钮
     // navigation: {
     //     nextEl: '.swiper-button-next',
     //     prevEl: '.swiper-button-prev',
     // },
-    
+
     // 如果需要滚动条
     // scrollbar: {
     //     el: '.swiper-scrollbar',
     // },
-})        
+})
+
+
+var mySwiperFour = new Swiper('.swiper-four', {
+    loop: true, // 循环模式选项
+
+    // 如果需要分页器
+    pagination: {
+        el: '.swiper-pagination',
+    },
+})
+
+var mySwiperFive = new Swiper('.swiper-five', {
+    loop: true,
+    loopAdditionalSlides: 3,
+
+    // 如果需要分页器
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    slidesPerView: 3.5,
+    grid: {
+        fill: 'column',
+        rows: 1,
+    },
+
+    //  如果需要前进后退按钮
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+
+    // 如果需要滚动条
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+})
+
