ec1fa629f967d4c393674b3cdb4a45286bf18926
ntl <1572679105@qq.com>
2023-03-17 19:35:46 +0800

    基本完成

--- components/header/header.js
+++ components/header/header.js
@@ -1,6 +1,15 @@
 $('.nav-block .nav ul li .drop-down-full ul #bjhq').click(function(){
     window.location.href='?p=bjhqdjq'
 })
-$('.nav-block .nav ul li .drop-down-full ul #maps').click(function(){
+
+$('.nav-block .nav ul li .drop-down-full ul #map').click(function(){
     window.location.href='?p=maps'
+})
+
+$('.nav-block .nav ul .two-explore .content-container ul .active ').click(function(){
+    window.location.href='?p=active'
+})
+
+$('.nav-block .nav ul .two-explore .content-container ul .playfun ').click(function(){
+    window.location.href='?p=playfun'
 })
---
+++ pages/active/active.js
@@ -0,0 +1,35 @@
+// 发送AJAX请求
+// 1.创建HTTP请求对象
+var xhr = new XMLHttpRequest()
+// 2.打开服务器的连接，设置要请求的接口
+xhr.open('GET', 'http://localhost:3000/v1/active/active', true)
+// 3.发送请求
+xhr.send()
+// 4.绑定事件，监听服务器端响应
+xhr.onload = function () {
+    // 获取服务器端响应的文本
+    var res = xhr.responseText
+    // console.log(res)
+    // 把JSON转为JS
+    var data = JSON.parse(res)
+    // 获取数组部分
+    // var arr = obj.data
+    console.log(data)
+    
+    $('.cont').html(data.data.map(value=>{
+        const {pic,title,topic}=value
+
+        return `
+        <li>
+            <div class="img"> 
+                <img src="${pic}" alt="">
+            </div>
+            <span>${title}</span>
+            <p>${topic}</p>
+        </li>
+        `
+    }))
+    
+    
+
+}
